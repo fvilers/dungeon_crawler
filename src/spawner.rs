12ac7dc6d288166a76a5a95da695baae123abac7
Fabian Vilers <fabian.vilers@dev-one.com>
2023-03-29 13:23:49 +0200

    Add monsters

--- src/spawner.rs
+++ src/spawner.rs
@@ -10,3 +10,19 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
         },
     ));
 }
+
+pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Point) {
+    ecs.push((
+        Enemy,
+        pos,
+        Render {
+            color: ColorPair::new(WHITE, BLACK),
+            glyph: to_cp437(match rng.range(0, 4) {
+                0 => 'E', // Ettin
+                1 => 'O', // Ogre
+                2 => 'o', // Orc
+                _ => 'g', // Goblin
+            }),
+        },
+    ));
+}
