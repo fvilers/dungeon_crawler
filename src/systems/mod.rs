ba7ef894bb57c4ba38df6a9f27e55d723f585bf8
Fabian Vilers <fabian.vilers@dev-one.com>
2023-03-30 20:12:24 +0200

    Add the random_move system

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -2,6 +2,7 @@ mod collisions;
 mod entity_render;
 mod map_render;
 mod player_input;
+mod random_move;
 
 use crate::prelude::*;
 
@@ -9,6 +10,8 @@ pub fn build_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
         .add_system(collisions::collisions_system())
+        .flush() // apply removed entities in the collisions system
+        .add_system(random_move::random_move_system())
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
         .build()
