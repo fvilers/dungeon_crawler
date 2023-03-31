22776be4aeca7d5030d0a316b06d98addaa3bb2c
Fabian Vilers <fabian.vilers@dev-one.com>
2023-03-31 09:59:38 +0200

    Add sending messages of intent

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -3,15 +3,16 @@ use bracket_lib::terminal::VirtualKeyCode;
 use legion::world::SubWorld;
 
 #[system]
-#[write_component(Point)]
+#[read_component(Point)]
 #[read_component(Player)]
 pub fn player_input(
     ecs: &mut SubWorld,
-    #[resource] map: &Map,
+    commands: &mut CommandBuffer,
     #[resource] key: &Option<VirtualKeyCode>,
-    #[resource] camera: &mut Camera,
     #[resource] turn_state: &mut TurnState,
 ) {
+    let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
+
     if let Some(key) = key {
         let delta = match key {
             VirtualKeyCode::Left => Point::new(-1, 0),
@@ -21,18 +22,17 @@ pub fn player_input(
             _ => Point::zero(),
         };
 
-        if delta.x != 0 || delta.y != 0 {
-            let mut players = <&mut Point>::query().filter(component::<Player>());
+        players.iter(ecs).for_each(|(entity, pos)| {
+            let destination = *pos + delta;
 
-            players.iter_mut(ecs).for_each(|pos| {
-                let destination = *pos + delta;
-
-                if map.can_enter_tile(destination) {
-                    *pos = destination;
-                    camera.on_player_move(destination);
-                    *turn_state = TurnState::PlayerTurn;
-                }
-            })
-        }
+            commands.push((
+                (),
+                WantsToMove {
+                    entity: *entity,
+                    destination,
+                },
+            ));
+        });
+        *turn_state = TurnState::PlayerTurn;
     }
 }
